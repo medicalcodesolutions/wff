554a0540acf273e16e6f3084206e383bdae04ab1
remonlapid <remonlapid@penguin>
2021-07-09 12:52:51 -0400

    WWF

--- src/com/fmb/wwf/client/WWF.java
+++ src/com/fmb/wwf/client/WWF.java
@@ -1,146 +1,174 @@
 package com.fmb.wwf.client;
 
-import com.fmb.wwf.shared.FieldVerifier;
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.TextArea;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
-public class WWF implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+public class WWF implements EntryPoint
+{
+	private FlexTable counts = new FlexTable ();
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
+	private class Letter
+	{
+		private String letter;
+		private int count;
+		
+		public Letter (String letter, int count)
+		{
+			this.letter = letter;
+			this.count = count;
 		}
 
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		public String getLetter ()
+		{
+			return letter + "&nbsp;&nbsp;&nbsp;";
+		}
+
+		public int getCount ()
+		{
+			return count;
+		}
+
+		public void setCount (int count)
+		{
+			this.count = count;
+		}
+	}
+
+	private ArrayList<Letter> remaining = new ArrayList<Letter> ();
+	private TextArea lettersPlayed;
+	
+	public void onModuleLoad ()
+	{
+		resetCounts ();
+		
+		lettersPlayed = TextArea.wrap (RootPanel.get ("lettersPlayed").getElement ());
+		RootPanel.get ("remaining").add (counts);
+		displayResults ();
+		
+		lettersPlayed.addKeyUpHandler (new KeyUpHandler () {
+
+			@Override
+			public void onKeyUp (KeyUpEvent event)
+			{
+				updateCounts ();
+			}});
+
+		lettersPlayed.addValueChangeHandler (new ValueChangeHandler<String> () {
+
+			@Override
+			public void onValueChange (ValueChangeEvent<String> event)
+			{
+				updateCounts ();
+			}});
+		
+		lettersPlayed.setFocus (true);
+	}
+	
+	private void updateCounts ()
+	{
+		String s = lettersPlayed.getText ().toUpperCase ().trim ();
+		for (int i = 0; i < s.length (); i++)
+		{
+			String l = s.substring (i, i + 1);
+			int index = 0;
+			
+			switch (l.charAt (0))
+			{
+			case '*':
+				break;
+				
+			case 'A':
+			case 'B':
+			case 'C':
+			case 'D':
+			case 'E':
+			case 'F':
+			case 'G':
+			case 'H':
+			case 'I':
+			case 'J':
+			case 'K':
+			case 'L':
+			case 'M':
+			case 'N':
+			case 'O':
+			case 'P':
+			case 'Q':
+			case 'R':
+			case 'S':
+			case 'T':
+			case 'U':
+			case 'V':
+			case 'W':
+			case 'X':
+			case 'Y':
+			case 'Z':
+				index = (l.charAt (0) - 'A' + 1);
+				break;
+				
+			default:
+				continue;
+			}
+
+			Letter letter = remaining.get (index) ;
+			int count = Math.max (letter.getCount () - 1, 0);
+			letter.setCount (count);
+		}	
+		
+		displayResults ();
+	}
+	private void resetCounts ()
+	{
+		remaining.clear ();
+		remaining.add (new Letter ("*", 2));
+		remaining.add (new Letter ("A", 9));
+		remaining.add (new Letter ("B", 2));
+		remaining.add (new Letter ("C", 2));
+		remaining.add (new Letter ("D", 5));
+		remaining.add (new Letter ("E", 13));
+		remaining.add (new Letter ("F", 2));
+		remaining.add (new Letter ("G", 3));
+		remaining.add (new Letter ("H", 4));
+		remaining.add (new Letter ("I", 8));
+		remaining.add (new Letter ("J", 1));
+		remaining.add (new Letter ("K", 1));
+		remaining.add (new Letter ("L", 4));
+		remaining.add (new Letter ("M", 2));
+		remaining.add (new Letter ("N", 5));
+		remaining.add (new Letter ("O", 8));
+		remaining.add (new Letter ("P", 2));
+		remaining.add (new Letter ("Q", 1));
+		remaining.add (new Letter ("R", 6));
+		remaining.add (new Letter ("S", 5));
+		remaining.add (new Letter ("T", 7));
+		remaining.add (new Letter ("U", 4));
+		remaining.add (new Letter ("V", 2));
+		remaining.add (new Letter ("W", 2));
+		remaining.add (new Letter ("X", 1));
+		remaining.add (new Letter ("Y", 2));
+		remaining.add (new Letter ("Z", 1));
+	}
+	
+	private void displayResults ()
+	{
+		for (int i = 0; i < remaining.size (); i++)
+		{
+			Letter letter = remaining.get (i);
+			counts.setWidget (0, i, new HTML (letter.getLetter ()));
+			int count = letter.getCount ();
+			counts.setWidget (1, i, new Label (count == 0 ? "" : count + ""));
+		}
+		
+		resetCounts ();
 	}
 }
